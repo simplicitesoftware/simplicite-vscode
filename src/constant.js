825caa14c1e4d6eb4f6356fe6dc0b74a3a9d34cb
ReineDeLaNight <germain40k@hotmail.fr>
2021-09-03 11:26:27 +0200

    extensionDependency added: redhat-java, changed path in constant.js for linux

--- src/constant.js
+++ src/constant.js
@@ -4,8 +4,8 @@ if (process.platform === 'win32'){
     TOKEN_SAVE_PATH = process.env.APPDATA + '/Code/User/globalStorage/simplicite-info.json';
     FILES_SAVE_PATH = process.env.APPDATA + '/Code/User/globalStorage/simplicite-files.json';
 } else {
-    TOKEN_SAVE_PATH = '/home/.vscode/simplicite-info.json';
-    FILES_SAVE_PATH = '/home/.vscode/simplicite-files.json';
+    TOKEN_SAVE_PATH = '/home/.vscode/extensions/simplicite-info.json';
+    FILES_SAVE_PATH = '/home/.vscode/extensions/simplicite-files.json';
 } 
 
 module.exports = {
