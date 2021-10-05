10f7f7b41c68fad57a7fb782066af142c945cd40
ReineDeLaNight <germain40k@hotmail.fr>
2021-10-05 18:07:16 +0200

    added new attribute moduleName

--- src/File.ts
+++ src/File.ts
@@ -4,11 +4,12 @@ export class File  {
     filePath: string;
     instanceUrl: string;
     workspaceFolderPath: string;
-
-    constructor (filePath: string, instanceUrl: string, workspaceFolderPath: string) {
+    moduleName: string;
+    constructor (filePath: string, instanceUrl: string, workspaceFolderPath: string, moduleName: string) {
         this.filePath = filePath;
         this.instanceUrl = instanceUrl;
         this.workspaceFolderPath = workspaceFolderPath;
+        this.moduleName = moduleName;
     }
 
     // SETTERS & GETTERS
