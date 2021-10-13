db9b371cdd54e1c9f6350896874d47969ca6c9d1
ReineDeLaNight <germain40k@hotmail.fr>
2021-10-13 18:19:26 +0200

    Updated BarItem

--- src/BarItem.ts
+++ src/BarItem.ts
@@ -15,48 +15,50 @@ export class BarItem {
         this.request = request;
     }
 
-    show (fileList: Array<File>, modules: Array<Module>, connectedInstancesUrl: Array<string>) {
-        this.barItem.tooltip = new MarkdownString(this.markdownGenerator(fileList, modules, connectedInstancesUrl));
+    show (modules: Array<Module>, connectedInstancesUrl: Array<string>) {
+        this.barItem.tooltip = new MarkdownString(this.markdownGenerator(modules, connectedInstancesUrl));
         this.barItem.show();
     }
     
-    markdownGenerator (fileList: Array<File>, modules: Array<Module>, connectedInstancesUrl: Array<string>) {
-        return this.urlModulesMarkdown(modules, connectedInstancesUrl) + '\n\n---\n\n' 
-        + this.modulesMarkdown(modules, connectedInstancesUrl);
+    markdownGenerator (modules: Array<Module>, connectedInstancesUrl: Array<string>) {
+        if (modules.length === 0 && connectedInstancesUrl.length === 0) {
+            return 'No Simplicite module detected';
+        }
+        return this.urlModulesMarkdown(modules, connectedInstancesUrl) + this.modulesMarkdown(modules, connectedInstancesUrl);
     }
 
     urlModulesMarkdown (modules: Array<Module>, connectedInstancesUrl: Array<string>) {
-        let moduleMarkdown = 'Connected Simplicite\'s instances and their corresponding modules:\n\n';
-        if (connectedInstancesUrl.length === 0) {
-            return moduleMarkdown + '- none\n\n';
-        }
-        for (let url of connectedInstancesUrl) {
-            moduleMarkdown += url + ':\n';
-            for (let module of modules) {
-                if (url === module.getInstanceUrl()) {
-                    moduleMarkdown += '- ' + module.getName() + '\n\n';
-                }
-            } 
+        let moduleMarkdown = '';
+        if (connectedInstancesUrl.length > 0) {
+            moduleMarkdown = 'Connected Simplicite\'s instances and their corresponding modules:\n\n';
+            for (let url of connectedInstancesUrl) {
+                moduleMarkdown += url + ':\n';
+                for (let module of modules) {
+                    if (url === module.getInstanceUrl()) {
+                        moduleMarkdown += '- ' + module.getName() + '\n\n';
+                    }
+                } 
+            }
+            moduleMarkdown += '\n\n---\n\n';
         }
         return moduleMarkdown;
     } 
 
     modulesMarkdown (modules: Array<Module>, connectedInstancesUrl: Array<string>) {
-        if (modules.length === 0) {
-            return '';
-        }
         let moduleMarkdown = '';
-        let firstTime = false;
-        for (let module of modules) {
-            if (!connectedInstancesUrl.includes(module.getInstanceUrl())) {
-                if (!firstTime) {
-                    moduleMarkdown += 'Disconnected modules:\n\n';
-                    firstTime = true;
+        let disconnectedModule = false;        
+        if (modules.length > 0) {
+            moduleMarkdown += 'Disconnected modules:\n\n';
+            for (let module of modules) {
+                if (!connectedInstancesUrl.includes(module.getInstanceUrl())) {
+                    moduleMarkdown += '- ' + module.getName() + '\n\n';
+                    disconnectedModule = true;
                 } 
-                moduleMarkdown += '- ' + module.getName() + '\n\n';
-            } 
+            }
+            if (!disconnectedModule) {
+                return;
+            }
         }
-
         return moduleMarkdown;
     }
 
