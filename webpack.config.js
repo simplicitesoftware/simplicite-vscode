7d2949ef0fe52d2698bd090644d98e5fea0191b5
germainaubert <germain40k@hotmail.fr>
2021-10-26 17:56:00 +0200

    new webpack configuration, adding .vscode folder

--- webpack.config.js
+++ webpack.config.js
@@ -13,64 +13,61 @@
 const path = require('path');
 const webpack = require('webpack');
 
-const webExtensionConfig = /** @type WebpackConfig */ {
-	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
-	target: 'webworker', // extensions run in a webworker context
-	entry: {
-		extension: './src/extension.ts' // source of the web extension main file
-	},
+const webConfig = /** @type WebpackConfig */ {
+	mode: "none", // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+	target: "webworker", // web extensions run in a webworker context
+	entry: "./src/extension.ts", // source of the web extension main file
 	output: {
-		filename: '[name].js',
-		path: path.join(__dirname, './dist/'),
-		libraryTarget: 'commonjs',
+		filename: "extension.js",
+		path: path.join(__dirname, "./dist/"),
+		libraryTarget: "commonjs",
 	},
 	resolve: {
-		mainFields: ['browser', 'module', 'main'], // look for `browser` entry point in imported node modules
-		extensions: ['.ts', '.js'], // support ts-files and js-files
+		mainFields: ["browser", "module", "main"], // look for `browser` entry point in imported node modules
+		extensions: [".ts", ".js"], // support ts-files and js-files
 		alias: {
-			// provides alternate implementation for node module and source files
+		// provides alternate implementation for node module and source files
 		},
 		fallback: {
-			"child_process": false,
-			"fs": false,
-			"copy-paste": false,
-			"os": false,
-			timers: require.resolve('timers-browserify'),
-			path: require.resolve("path-browserify"),
-			http: require.resolve("stream-http"),
-			zlib: require.resolve("browserify-zlib"),
-			https: require.resolve("https-browserify"),
-			url: require.resolve("url/"),
-			stream: require.resolve("stream-browserify")
+		// Webpack 5 no longer polyfills Node.js core modules automatically.
+		// see https://webpack.js.org/configuration/resolve/#resolvefallback
+		// for the list of Node.js core module polyfills.
+		assert: require.resolve("assert"),
+		fs: require.resolve('path-browserify'),
+		os: require.resolve("os-browserify/browser"),
+		stream: require.resolve("stream-browserify"),
+		path: require.resolve("path-browserify"),
+		https: require.resolve("https-browserify"),
+		timers: require.resolve("timers-browserify"),
+		http: require.resolve("stream-http"),
+		zlib: require.resolve("browserify-zlib")
 		},
 	},
 	module: {
 		rules: [
+		{
+			test: /\.ts$/,
+			exclude: /node_modules/,
+			use: [
 			{
-				test: /\.ts$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: 'ts-loader',
-					},
-				],
+				loader: "ts-loader",
 			},
+			],
+		},
 		],
 	},
 	plugins: [
 		new webpack.ProvidePlugin({
-			process: 'process/browser', // provide a shim for the global `process` variable
+		process: "process/browser", // provide a shim for the global `process` variable
 		}),
 	],
 	externals: {
-		vscode: 'commonjs vscode', // ignored because it doesn't exist
+		vscode: "commonjs vscode", // ignored because it doesn't exist
 	},
 	performance: {
 		hints: false,
 	},
-	devtool: 'nosources-source-map', // create a source map that points to the original source file'
+	devtool: "nosources-source-map", // create a source map that points to the original source file
 };
 
-module.exports = [
-  webExtensionConfig
-];
+module.exports = [webConfig];
