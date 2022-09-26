eb3ff5a4f23129d617e415cebbc09fd7a5566859
aubert <aubert@et.intechinfo.fr>
2022-09-26 18:14:43 +0200

    Fixed save all bug

--- src/web/FileService.ts
+++ src/web/FileService.ts
@@ -1,53 +1,40 @@
 'use strict';
 
-import { url } from 'inspector';
 import { workspace, TextDocument, Uri, TextDocumentChangeEvent } from 'vscode';
 import { File } from './File';
 import { FileInstance } from './interfaces';
-import { logger } from './Log';
 import { SimpliciteInstanceController } from './SimpliciteInstanceController';
 
 // listens to saved files.
 // compatible with the vscode feature save all
-export class FileService {
-	private modifiedFiles: Map<string, Map<string, File>>; // key = url, modified simplicité files associated with their instances
-	private savedCpt: number; // not used when send on save option is disabled
-	private simpliciteInstanceController: SimpliciteInstanceController;
-	constructor (simpliciteInstanceController: SimpliciteInstanceController) {
-		this.modifiedFiles = new Map();
-		this.savedCpt = 0;
-		this.simpliciteInstanceController = simpliciteInstanceController;
-	}
+export async function fileService(simpliciteInstanceController: SimpliciteInstanceController) {
+	let modifiedFiles: Map<string, Map<string, File>> = new Map();; // key = url, modified simplicité files associated with their instances
+	let savedCpt: number = 0; // not used when send on save option is disabled
 
-	async fileListener() {
-		// get a trace of simplicité modified files
-		workspace.onDidChangeTextDocument((doc: TextDocumentChangeEvent) => {
-			const fileUrl = this.simpliciteInstanceController.getFileAndInstanceUrlFromPath(doc.document.uri);
-			if(!fileUrl) return;
-			this.setModifiedFile(fileUrl);
-		});
+	// get a trace of simplicité modified files
+	workspace.onDidChangeTextDocument((doc: TextDocumentChangeEvent) => {
+		const fileUrl = simpliciteInstanceController.getFileAndInstanceUrlFromPath(doc.document.uri);
+		if(!fileUrl) return;
+		setModifiedFile(fileUrl);
+	});
 
-		// check if saved file is in trace of modified files
-		workspace.onDidSaveTextDocument(this.onDidSaveTextDocument);
-		
-	}
-
-	private async onDidSaveTextDocument(doc: TextDocument) {
-		const file = this.getModifiedFile(doc.uri);
+	// check if saved file is in trace of modified files
+	workspace.onDidSaveTextDocument(async (doc: TextDocument) => {
+		const file = getModifiedFile(doc.uri);
 		if(!file) return;
 		await file.saveFileAsTracked();
-		++this.savedCpt;
-		if(this.savedCpt === this.countModifiedFiles()) { // if all files have been saved
-			if(workspace.getConfiguration('simplicite-vscode-tools').get('api.sendFileOnSave')) await this.simpliciteInstanceController.sendAllFiles();
-			this.savedCpt = 0;
-			this.modifiedFiles = new Map();
+		++savedCpt;
+		if(savedCpt === countModifiedFiles()) { // if all files have been saved
+			if(workspace.getConfiguration('simplicite-vscode-tools').get('api.sendFileOnSave')) await simpliciteInstanceController.sendAllFiles();
+			savedCpt = 0;
+			modifiedFiles = new Map();
 		}
-	}
+	});
 
-	private setModifiedFile(fileUrl: FileInstance): void {
-		const files = this.modifiedFiles.get(fileUrl.url);
+	function setModifiedFile(fileUrl: FileInstance): void {
+		const files = modifiedFiles.get(fileUrl.url);
 		if(!files) {
-			this.modifiedFiles.set(fileUrl.url, (new Map()).set(fileUrl.file.uri.path.toLowerCase(), fileUrl.file));
+			modifiedFiles.set(fileUrl.url, (new Map()).set(fileUrl.file.uri.path.toLowerCase(), fileUrl.file));
 		} else {
 			const path = fileUrl.file.uri.path;
 			if(!files.has(path)) {
@@ -56,9 +43,9 @@ export class FileService {
 		}
 	}
 
-	private getModifiedFile(uri: Uri): File | undefined {
+	function getModifiedFile(uri: Uri): File | undefined {
 		// loop on instances
-		const value = this.modifiedFiles.values();
+		const value = modifiedFiles.values();
 		for(const instance of value) {
 			const file = instance.get(uri.path.toLowerCase());
 			if(file) return file;
@@ -66,14 +53,22 @@ export class FileService {
 		return undefined;
 	}
 
-	private countModifiedFiles(): number {
+	function countModifiedFiles(): number {
 		let x = 0;
-		const value = this.modifiedFiles.values();
+		const value = modifiedFiles.values();
 		for(const instance of value) {
 			x += instance.size;
 		}
 		return x;
 	}
+}
+
+	
+
+	
+
+
+	
 
 	// 	// create temp folder and copy files to store the initial state of a file (for conflict resolution)
 	// 	async initTempFolder(fileModule: FileAndModule[]) {
@@ -94,4 +89,3 @@ export class FileService {
 	// 			logger.warn(e);
 	// 		}
 	// 	}
-}
